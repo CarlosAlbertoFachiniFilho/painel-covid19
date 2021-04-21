adf31e35719f6d36d4c1d3b615f73e2d298a3d76
Carlos Alberto Fachini Filho <carlosalbertofachinifilho@gmail.com>
2021-04-20 22:48:22 -0300

    Criado Header / Title e Dropdown com nome dos paises

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [countries, setcountries] = useState([]);
+  const [country, setcountry] = useState("Worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("data:", data);
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso3,
+          }));
+          setcountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Painel covid 19</h1>
-
-      {/* header */}
-      {/* Title - select input dropdown field */}
+      <div className="app_header">
+        <h1>Painel covid 19</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant="outlined"
+            value={country}
+            onChange={(e) => setcountry(e.target.value)}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem key={country.value} value={country.name}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* InfoBox */}
       {/* InfoBox */}
